fbfa0dc505e9096ccacacae92962ba05677f00c2
scrawach <scrawach@gmail.com>
2023-09-15 11:54:36 +0300

    upload example gif

--- addons/FileAccessWeb/examples/file_access_web_image_example.gd
+++ addons/FileAccessWeb/examples/file_access_web_image_example.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	file_access_web.progress.connect(_on_progress)
 
 func _on_upload_pressed() -> void:
-	file_access_web.open("image/png")
+	file_access_web.open(".jpg")
 
 func _on_progress(current_bytes: int, total_bytes: int) -> void:
 	var percentage: float = float(current_bytes) / float(total_bytes) * 100
